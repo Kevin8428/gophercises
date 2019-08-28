95081638ec669e91f4465e632d601ed38d619fb8
Kevin Deutscher <kevin@dispatch.me>
2019-08-27 23:40:03 -0400

    one

--- 01/main.go
+++ 01/main.go
@@ -8,14 +8,43 @@ import (
 	"log"
 	"os"
 	"strings"
+	"time"
 )
 
+func getArgs() map[string]string {
+	m := map[string]string{}
+	for _, v := range os.Args {
+		if strings.Contains(v, "file=") {
+			m["file"] = strings.SplitAfter(v, "file=")[1]
+		} else if strings.Contains(v, "time=") {
+			m["time"] = strings.SplitAfter(v, "time=")[1]
+		}
+	}
+	return m
+}
+
 func main() {
-	file, _ := os.Open("one.csv")
+	m := getArgs()
+	var f string
+	if val, ok := m["file"]; ok {
+		f = val
+	} else {
+		f = "one.csv"
+	}
+
+	file, _ := os.Open(f)
 	r := csv.NewReader(bufio.NewReader(file))
 	reader := bufio.NewReader(os.Stdin)
 	correct := 0
 	incorrect := 0
+	fmt.Print("hit enter to start timer")
+	reader.ReadString('\n')
+	go func() {
+		time.Sleep(time.Second * 5)
+		fmt.Println("\ncorrect: ", correct)
+		fmt.Println("incorrect: ", incorrect)
+		os.Exit(0)
+	}()
 	for {
 		line, error := r.Read()
 		if error == io.EOF {
